6777db99ea0ab9e7ffa99e97a7b16278290f29b5
shs3182ym <yeongmins030@gmail.com>
2020-10-01 10:05:56 +0900

    usage/s/2

--- bot/Bot.js
+++ bot/Bot.js
@@ -18,7 +18,7 @@ function sendEmbed(res, color, title, desc){
     .setTitle(title)
     .setDescription(desc)
     .setFooter(`Po₂₁₀ Bot ${ver}`)
-  res.channel.send(tmp) 
+  res.channel.send(tmp)
 }
 
 client.on("ready", () => {
@@ -29,11 +29,12 @@ client.on("message", res => {
   var msg = res.content;
   var argv = msg.split(" ");
   switch (argv[0]) {
-    case "usages":
+    case "po!usages":
       var ram = `${(usageObj.memory / 1048576).split(".")[0]} MiB + ${((usageObj.memory - (usageObj.memory / 1048576 * 1048576)) / 1024).split(".")[0]} KiB`
       var vmm = `${(usageObj.memoryInfo.vsize / 1048576).split(".")[0]} MiB + ${((usageObj.memoryInfo.vsize - (usageObj.memoryInfo.vsize / 1048576 * 1048576)) / 1024).split(".")[0]} KiB`
       var text = `CPU 사용량: ${usageObj.cpu}\nRAM 사용량: ${ram}\n가상 메모리 사용량: ${vmm}`
       sendEmbed(res, 0x00FF00, "성공 - 사용량 출력", text)
+      break
   }
 });
 
