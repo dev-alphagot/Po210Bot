5f46d5e30e99caa65a77de1a55de128211bc7425
shs3182ym <yeongmins030@gmail.com>
2020-10-03 18:56:51 +0900

    .

--- statusModule/moneyManager.js
+++ statusModule/moneyManager.js
@@ -1,33 +1,28 @@
 var prefix = {
-    "default":"곶",
-    "prefix":[
-        "",
-        "만",
-        "억",
-        "조",
-        "경",
-        "해",
-        "자",
-        "정",
-        "재",
-        "극"
-    ]
-}
-
-function moneyToString(raw){
-    if(raw > 9999){
-        let j
-        for(var i = 1; i < 10; i++){
-            j = raw / Math.pow(10000, i)
-            if(j < 1){
-                return Math.floor(j * 10000) + " " + prefix.prefix[i - 1] + " " + moneyToString(raw - Math.floor(j * Math.pow(10000, i)))
-            }
+    default: "곶",
+    prefix: ["", "만", "억", "조", "경", "해", "자", "정", "재", "극"]
+  };
+  
+  function moneyToString(raw) {
+    if (raw > 9999) {
+      let j;
+      for (var i = 1; i < 10; i++) {
+        j = raw / Math.pow(10000, i);
+        if (j < 1) {
+          return (
+            Math.floor(j * 10000) +
+            prefix.prefix[i - 1] +
+            " " +
+            moneyToString(raw - Math.floor(j * Math.pow(10000, i)))
+          );
         }
-    }else{
-        return raw + " " + "곶"
+      }
+    } else {
+      return raw + " " + "곶";
     }
-}
-
-module.exports = {
+  }
+  
+  module.exports = {
     moneyToString
-}
+  };
+  
